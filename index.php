009b0dd7bf2ceb9179ae1c6728e1737519436cb4
ezequiel hernandez <32853621+drandroidsv@users.noreply.github.com>
2023-03-31 22:33:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,36 +8,17 @@ $imagecount = count(glob("*/*.{jpg,png,gif,jpeg}", GLOB_BRACE));
 $arr =  array(
     "info" => array (
         "Title" => "Pixel Wallpapers", 
-        "Api Link" => "https://github.com/wacko1805/google-pixel-wallpapers", 
-        "Wallpapers Link" => "https://github.com/wacko1805/google-pixel-wallpapers-images", 
-        "Created by" => "Jack Sam",
+        "Api Link" => "https://github.com/drandroidsv/wallpapermain", 
+        "Wallpapers Link" => "https://github.com/drandroidsv/Wallia", 
+        "Created by" => "Agdala",
         "Imagecount" => "$imagecount",
     ));
 echo "[";
 echo json_encode($arr);
 echo ",";
 include('motif.php');
-include('pkl.php');
-include('P6l.php');
-include('P6a.php');
-include('p6.php');
-include('a12.php');
-include('puw.php');
-include('p5a.php');
-include('p5.php');
-include('p4.php');
-include('p4a.php');
-include('pride.php');
-include('hhm.php');
-include('iwd.php');
-include('bhm.php');
-include('ed.php');
-include('AAPI.php');
-include('ifd.php');
-include('ipan.php');
-include('aosp.php');
-include('ac.php');
-include('sop.php');
+include('carros.php');
+include('coloridos.php');
 echo '{"":""}';
 echo "]";
 ?>
