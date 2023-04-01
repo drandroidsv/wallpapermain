0e13a14f6503e079a47e6fb1b77d94d847d345c3
ezequiel hernandez <32853621+drandroidsv@users.noreply.github.com>
2023-04-01 01:34:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ $arr =  array(
 echo "[";
 echo json_encode($arr);
 echo ",";
+include('carros.php');
+include('coloridos.php');
+include('animal.php');
 include('motif.php');
 include('pkl.php');
 include('P6l.php');
